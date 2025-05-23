012aaa0740241e30387de953e63117513d8a31ad
mahadihasandev <mayhaydihasan.com@gmail.com>
2025-05-23 13:00:05 +0600

    Container component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 
 import Image from "./component/Image"
 import Banner from './assets/banner.jpg'
+import Container from "./component/Container"
 
 const App = () => {
   return (
     <>
-  
+  <Container>
+    <h1>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, dicta et eum nihil laboriosam placeat corporis delectus culpa quas explicabo neque. Magnam est quaerat alias inventore, consequuntur, facere nisi placeat laborum sapiente architecto provident, temporibus ex? Quam asperiores in enim, nulla ullam et, deleniti blanditiis minima nisi doloremque unde! Earum officiis est fuga architecto error sit facere esse, nam eos eveniet reiciendis veritatis aliquid eum laudantium voluptate! Quo aliquam laboriosam, enim rerum repellat aut, unde, totam eveniet possimus quam natus rem fugit velit fugiat nostrum dolor. Ea minus sapiente sequi. Perspiciatis explicabo ex quis esse ipsum quaerat sapiente blanditiis libero natus obcaecati recusandae voluptas vel molestiae atque molestias, quia, in, enim iste sequi aut nostrum aliquam fugiat dolorum deserunt. A recusandae minus eius cumque, alias voluptatibus facilis consectetur veniam at, et sint, placeat accusamus aliquam repellat nihil. Dolore pariatur vitae iure? Eum minima voluptatibus porro quae et ipsum unde, illum aliquam rerum quibusdam, maiores cum itaque est iste vitae quis adipisci ipsam incidunt ad? Ut aliquam excepturi labore, rem facilis consequatur odit quidem quis nemo sed in provident blanditiis, sit nulla totam, ducimus assumenda ullam veritatis. Cupiditate sunt fuga iste ipsum laudantium quisquam aspernatur explicabo, molestiae qui nam magni laboriosam.
+      
+    </h1>
+  </Container>
  
     </>
   )
---
+++ src/component/Container.jsx
@@ -0,0 +1,9 @@
+
+
+function Container({children}) {
+  return (
+    <div className="max-w-container mx-auto">{children}</div>
+  )
+}
+
+export default Container
